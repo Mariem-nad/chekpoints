b1cd00e9c33f624039dd6ea67a303b88fa0c9afc
Mariem-nad <153771066+Mariem-nad@users.noreply.github.com>
2024-04-06 16:38:57 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbarr from './components/Navbarr';
+import Cardss from './components/Cardss';
+import Forms from './components/Forms';
+function App() {
+  return (
+    <div className="App">
+     <Navbarr/>
+     <Cardss/>
+     <Forms/>
+    </div>
+  );
+}
+
+export default App;
