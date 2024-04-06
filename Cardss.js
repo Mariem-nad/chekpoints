0a8adce3279b3887fad473cfd6ec4c531c7828e6
Mariem-nad <153771066+Mariem-nad@users.noreply.github.com>
2024-04-06 16:40:09 +0200

    Add files via upload

---
+++ Cardss.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+function Cardss() {
+  return (
+    <div>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://imgs.search.brave.com/6LOADaHkMw84QEwMn5jKl-ciQMgT47CKrxtLxDXbspU/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/cGhvdG93YXJlaG91/c2UuY28ubnovYXNz/ZXRzL25ld3MtaW1h/Z2VzL1Bhc3Nwb3J0/LVBob3Rvcy5qcGc_/dGltZT0xNjczNjM5/ODA0JnRpdGxlPVBh/c3Nwb3J0K1Bob3Rv/cw" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://imgs.search.brave.com/o3dchWaBdOYg73rLCpA1Mrte5K7D-3Cam6iwWd8lLoY/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/MDQxOTQxMDQ0MDQt/NDMzMTgwNzczMDE3/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4TWpCOGZH/WnlaV1Y4Wlc1OE1I/eDhNSHg4ZkRBPQ" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://imgs.search.brave.com/dCwvoUTm3FMlZCZ-v2BnPN2cTA-Z5l2mTwIfgJWsH3Q/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by9m/dWxsLXNob3QtY291/cGxlLXdhbGtpbmct/b3V0ZG9vcnNfMjMt/MjE0OTgyOTYxMC5q/cGc_c2l6ZT02MjYm/ZXh0PWpwZw" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}
+
+export default Cardss
